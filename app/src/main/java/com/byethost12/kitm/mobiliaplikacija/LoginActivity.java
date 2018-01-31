d4f378550c6a6a6621a63fa123995aa8b3eff08c
algirdaskuslys <algirdas.kuslys@kitm.lt>
2018-01-31 12:31:26 +0200

    pridetas isiminti mane funkcionalumas.

--- app/src/main/java/com/byethost12/kitm/mobiliaplikacija/LoginActivity.java
+++ app/src/main/java/com/byethost12/kitm/mobiliaplikacija/LoginActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
@@ -18,10 +20,22 @@ public class LoginActivity extends Activity {
         Button btnLogin = (Button) findViewById(R.id.login);
         final EditText etUsername = (EditText) findViewById(R.id.username);
         final EditText etPassword = (EditText) findViewById(R.id.password);
+        final CheckBox cbRememberMe = (CheckBox)findViewById(R.id.rememberMe);
+
+        final User user = new User(LoginActivity.this);
 
         etUsername.setError(null);
         etPassword.setError(null);
 
+        cbRememberMe.setChecked(user.isRemembered());
+
+    if(user.isRemembered()) {
+        etUsername.setText(user.getUsernameForLogin(), TextView.BufferType.EDITABLE);
+        etPassword.setText(user.getPasswordForLogin(), TextView.BufferType.EDITABLE);
+    }else{
+        etUsername.setText("", TextView.BufferType.EDITABLE);
+        etPassword.setText("", TextView.BufferType.EDITABLE);
+    }
 
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +60,15 @@ public class LoginActivity extends Activity {
                     Toast.makeText(LoginActivity.this,
                             "Prisijungimo vardas: " + etUsername.getText().toString() + "\n" +
                                     "Slaptažodis: " + etPassword.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                    user.setUsernameForLogin(etUsername.getText().toString());
+                    user.setPasswordForLogin(etPassword.getText().toString());
+                    if(cbRememberMe.isChecked()){
+                        user.setRememberMeKeyForLogin(true);
+                    }else{
+                        user.setRememberMeKeyForLogin(false);
+                    }
+
                     Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);
                     startActivity(goToSearchActivity);
                 }
---
+++ app/src/main/java/com/byethost12/kitm/mobiliaplikacija/User.java
@@ -0,0 +1,70 @@
+package com.byethost12.kitm.mobiliaplikacija;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by mokytojas on 2018-01-31.
+ */
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private static final String PREFERENCES_FILE_NAME = "com.byethost12.kitm.mobiliaplikacija";
+    private static final String USERNAME_KEY = "username";
+    private static final String PASSWORD_KEY = "password";
+    private static final String REMEMBER_ME_KEY = "rememberMe";
+
+    private SharedPreferences sharedPreferences;
+
+    //skirtas register activity registruojant nauja vartotoja
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    //konstruktorius skirtas login langui
+    public User(Context context){
+        this.sharedPreferences = context.getSharedPreferences
+                (User.PREFERENCES_FILE_NAME,Context.MODE_PRIVATE);
+    }
+//geteriai seteriai skirti login activity prisijungimo langui
+    public String getUsernameForLogin() {
+        return this.sharedPreferences.getString(USERNAME_KEY,"");
+    }
+    public void setUsernameForLogin(String username) {
+        this.sharedPreferences.edit().putString(USERNAME_KEY,username).commit();
+    }
+    public String getPasswordForLogin() {
+        return this.sharedPreferences.getString(PASSWORD_KEY,"");
+    }
+    public void setPasswordForLogin(String password) {
+        this.sharedPreferences.edit().putString(PASSWORD_KEY,password).commit();
+    }
+    public boolean isRemembered(){
+        return this.sharedPreferences.getBoolean(REMEMBER_ME_KEY,false);
+    }
+    public void setRememberMeKeyForLogin(boolean rememberMe){
+      this.sharedPreferences.edit().putBoolean(REMEMBER_ME_KEY,rememberMe).commit();
+    }
+    //sitie geteriai seteriai skirti register activity langui
+    public String getUsernameForRegister() {
+        return username;
+    }
+    public void setUsernameForRegister(String username) {
+        this.username = username;
+    }
+    public String getPasswordForRegister() {
+        return password;
+    }
+    public void setPasswordForRegister(String password) {
+        this.password = password;
+    }
+    public String getEmailForRegister() {
+        return email;
+    }
+    public void setEmailForRegister(String email) {
+        this.email = email;
+    }
+}
