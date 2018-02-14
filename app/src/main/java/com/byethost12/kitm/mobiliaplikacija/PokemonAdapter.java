b6271688331a97b1a19391dd4d56195254b89fff
algirdaskuslys <algirdas.kuslys@kitm.lt>
2018-02-14 13:18:22 +0200

    Pridėta paieška duomenų bazėje SQLite (neištestuota).

---
+++ app/src/main/java/com/byethost12/kitm/mobiliaplikacija/PokemonAdapter.java
@@ -0,0 +1,89 @@
+package com.byethost12.kitm.mobiliaplikacija;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by mokytojas on 2018-02-14.
+ */
+
+public class PokemonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private Context context;
+    private LayoutInflater inflater;
+    private List<Pokemonas> pokemonai = Collections.emptyList();
+    private Pokemonas currentPokemon;
+
+    public static final String ENTRY = "com.byethost12.kitm.mobiliaplikacija";
+
+    //konstruktorius reikalingas susieti
+    // esama langa ir perduoti sarasa pokemonui is DB
+    public PokemonAdapter(Context context,List<Pokemonas> pokemonai){
+        this.context = context;
+        this.pokemonai = pokemonai;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    //inflate the layout when viewholder is created
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.container_pokemon,parent,false);
+        MyHolder holder = new MyHolder(view);
+        return holder;
+    }
+
+    @Override
+    //bind data
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        //get current position of item in
+        // recyclerview to bind data and assign value from list
+        MyHolder myHolder = (MyHolder) holder;
+        currentPokemon = pokemonai.get(position);
+        myHolder.tvPavadinimas.setText(           currentPokemon.getName());
+        myHolder.tvTipas.setText("Tipas: "      + currentPokemon.getType());
+        myHolder.tvGalia.setText("Galia: "      + currentPokemon.getCp());
+        myHolder.tvSavybes.setText("Savybės: "  + currentPokemon.getAbilities());
+    }
+
+    @Override
+    public int getItemCount() {
+        return pokemonai.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView tvPavadinimas,tvTipas,tvGalia,tvSavybes;
+
+        public MyHolder(View itemView){
+            super(itemView);
+            tvPavadinimas = (TextView)itemView.findViewById(R.id.pavadinimas);
+            tvTipas = (TextView)itemView.findViewById(R.id.tipas);
+            tvGalia = (TextView)itemView.findViewById(R.id.galia);
+            tvSavybes = (TextView)itemView.findViewById(R.id.savybes);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            int itemPosition = getAdapterPosition();
+            int pokemonaiID = pokemonai.get(itemPosition).getId();
+
+            // TODO: siųsti pasirinkto pokemono objektą vietoj id
+            Pokemonas pokemonas = pokemonai.get(itemPosition);
+
+            Intent intent = new Intent(context, NewEntryActivity.class);
+
+            intent.putExtra(ENTRY, pokemonaiID);
+            context.startActivity(intent);
+        }
+    }
+}
+
+
--- app/src/main/java/com/byethost12/kitm/mobiliaplikacija/SearchActivity.java
+++ app/src/main/java/com/byethost12/kitm/mobiliaplikacija/SearchActivity.java
@@ -1,14 +1,28 @@
 package com.byethost12.kitm.mobiliaplikacija;
 
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     Button btnPrideti;
+    SearchView searchView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +35,93 @@ public class SearchActivity extends AppCompatActivity {
         btnPrideti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SearchActivity.this,NewEntryActivity.class);
+                Intent intent = new Intent(SearchActivity.this, NewEntryActivity.class);
                 startActivity(intent);
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //adds item to actionbar
+        getMenuInflater().inflate(R.menu.search, menu);
+        //get search item from actionbar and get search service
+        MenuItem searchitem = menu.findItem(R.id.actionSearch);
+        SearchManager searchManger = (SearchManager) SearchActivity.this.getSystemService(Context.SEARCH_SERVICE);
+        if (searchitem != null) {
+            searchView = (SearchView) searchitem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManger.getSearchableInfo(SearchActivity.this.getComponentName()));
+            searchView.setIconified(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    //every time when you press search button an actvity is recreated which in turn
+    //calls this function
+    protected void onNewIntent(Intent intent) {
+        //get search query and create object of class AsyncFetch
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (searchView != null) {
+                searchView.clearFocus();
+            }
+            new AsyncFetch(query).execute();
+        }
+    }
+
+    class AsyncFetch extends AsyncTask<String, String, String> {
+        ProgressDialog progressDialog = new ProgressDialog(SearchActivity.this);
+        String searchQuery;
+        List<Pokemonas> pokemonai = Collections.emptyList();
+
+        public AsyncFetch(String searchQuery) {
+            this.searchQuery = searchQuery;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Prašome palaukti");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            DatabaseSQLite db = new DatabaseSQLite(SearchActivity.this);
+
+            // TODO: 2 laboro užduotis
+            pokemonai = db.getAllPokemonai();
+            if (pokemonai.isEmpty()) {
+                return "no rows";
+            } else {
+                return "rows";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progressDialog.dismiss();
+            if (result.equals("no rows")) {
+                Toast.makeText(SearchActivity.this, "Pagal paiešką nerasta duomenų", Toast.LENGTH_LONG).show();
+            } else {
+                //setup and hand over list pokemonai to recyclerview
+                RecyclerView rvPokemonai = (RecyclerView) findViewById(R.id.pokemon_list);
+                PokemonAdapter pokemonAdapter = new PokemonAdapter(SearchActivity.this, pokemonai);
+                rvPokemonai.setAdapter(pokemonAdapter);
+                rvPokemonai.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
+
+            }
+        }
+    }
 }
+
